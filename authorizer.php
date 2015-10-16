a4adf645f2f25a9d9eb033b9ec31bdb5794c8834
Mark Kimsal <metrofindings@gmail.com>
2015-10-16 16:18:50 -0400

    Remove static kernel call in favor of injection.

--- authorizer.php
+++ authorizer.php
@@ -8,10 +8,11 @@ class Metrou_Authorizer {
 	/**
 	 * Fire access.denied event if user is anonymous
 	 */
-	public function requireLogin($request, $response, $user) {
+	public function requireLogin($request, $response, $user, $kernel) {
 		if ($user->isAnonymous()) {
 			$request->unauthorized = TRUE;
-			Metrofw_Kernel::emit('access.denied', $user);
+			$kernel->signal('access.denied', $user);
+//			Metrofw_Kernel::emit('access.denied', $user);
 		}
 	}
 }
