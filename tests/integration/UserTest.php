db7b7c41bcb64b1593de3da5d462702092c18446
Mark Kimsal <metrofindings@gmail.com>
2015-10-16 16:38:16 -0400

    Add tests for double registration protection.

--- tests/integration/UserTest.php
+++ tests/integration/UserTest.php
@@ -34,6 +34,63 @@ class Metrou_Intg_Tests_User extends PHPUnit_Framework_TestCase {
 		$this->assertTrue( $this->user->userId > 0 );
 	}
 
+
+	public function test_save_duplicate_new_user_fails() {
+
+		//should NOT read array key 'loggedIn'
+		$this->user->populate(
+			array(
+				'username'  => 'testuser',
+				'password'  => 'testpassword',
+				'email'     => 'test@localhost',
+				'loggedIn'  => TRUE,
+				'logged_in' => TRUE
+			)
+		);
+		$this->user->hashPassword();
+		$pkey = $this->user->save();
+		$this->assertFalse( !$pkey );
+		$this->assertTrue( $this->user->userId > 0 );
+
+		$duplicateUser = new Metrou_User();
+		//should NOT read array key 'loggedIn'
+		$duplicateUser->populate(
+			array(
+				'username'  => 'testuser',
+				'password'  => 'testpassword',
+				'email'     => 'test@localhost',
+				'loggedIn'  => TRUE,
+				'logged_in' => TRUE
+			)
+		);
+		$duplicateUser->hashPassword();
+		$pkey = $duplicateUser->save();
+		$this->assertFalse( $pkey );
+	}
+
+	public function test_save_user_twice_doesnt_double_register() {
+
+		//should NOT read array key 'loggedIn'
+		$this->user->populate(
+			array(
+				'username'  => 'testuser',
+				'password'  => 'testpassword',
+				'email'     => 'test@localhost',
+				'loggedIn'  => TRUE,
+				'logged_in' => TRUE
+			)
+		);
+		$this->user->hashPassword();
+		$pkey = $this->user->save();
+		$this->assertFalse( !$pkey );
+		$this->assertTrue( $this->user->userId > 0 );
+
+		$pkey = $this->user->save();
+		$this->assertFalse( !$pkey );
+		$this->assertTrue( $pkey > 0 );
+	}
+
+
 	public function test_save_new_is_not_active() {
 		$this->user->isAnonymous();
 
