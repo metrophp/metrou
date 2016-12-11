30be89cc0ca1163fdac87bf84e32fd7770eb802d
Mark Kimsal <metrofindings@gmail.com>
2016-12-11 12:11:33 -0500

    Add whitelist and blacklist support to authorizer

--- README.md
+++ README.md
@@ -2,15 +2,20 @@ Metro User Library
 
 Sample Config
 ```php
-_iCanHandle('authenticate', 'metrou/authenticator.php');
-_iCanHandle('authorize',    'metrou/authorizer.php::requireLogin');
 
-//events
-_iCanHandle('access.denied',        'metrou/login.php::accessDenied');
-_iCanHandle('authenticate.success', 'metrou/login.php::authSuccess');
-_iCanHandle('authenticate.failure', 'metrou/login.php::authFailure');
-
-//things
 _didef('user',           'metrou/user.php');
 _didef('session',        'metrou/sessiondb.php');
+
+_didef('authorizer', 'metrou/authorizer.php',
+   	array('metrou', '/login', '/dologin', '/logout', '/dologout', '/register')
+);
+$authorizer = _make('authorizer');
+_connect('authorize',            $authorizer);
+_connect('authenticate',        'metrou/authenticator.php');
+
+//events
+_connect('access.denied',        'metrou/login.php::accessDenied');
+_connect('authenticate.success', 'metrou/login.php::authSuccess');
+_connect('authenticate.failure', 'metrou/login.php::authFailure');
+
 ```
--- authorizer.php
+++ authorizer.php
@@ -5,16 +5,72 @@
  */
 class Metrou_Authorizer {
 
+	public $whiteList = [];
+	public $blackList = [];
+
+	public function __construct($whiteList=array(), $blackList=array()) {
+		$this->whiteList = $whiteList;
+		$this->blackList = $blackList;
+	}
+
+	public function whitelistApp($app) {
+		$this->whiteList[] = $app;
+	}
+
+	public function whitelistUrl($url) {
+		$this->whiteList[] = $url;
+	}
+
+	public function blacklistApp($app) {
+		$this->blackList[] = $app;
+	}
+
+	public function blacklistUrl($url) {
+		$this->blackList[] = $url;
+	}
+
+	public function authorize($request, $response, $user, $kernel) {
+		return $this->requireLogin($request, $response, $user, $kernel);
+	}
+
 	/**
 	 * Fire access.denied event if user is anonymous
 	 */
 	public function requireLogin($request, $response, $user, $kernel) {
+		if ( in_array($request->appName , $this->whiteList) ) {
+			$request->unauthorized = FALSE;
+			return;
+		}
+		$args = array(
+			'request'  => $request,
+			'response' => $response,
+			'user'     => $user
+		);
+
+		//if any white list is the first part of the requestedUrl
+		foreach ($this->whiteList as $_w) {
+			if (strpos($request->requestedUrl, $_w) === 0) {
+				$request->unauthorized = FALSE;
+				return;
+			}
+		}
+
+		if ( in_array($request->appName , $this->blackList) ) {
+			$request->unauthorized = TRUE;
+			$kernel->signal('access.denied', $user, $args);
+			return;
+		}
+		//if any black list is the first part of the requestedUrl
+		foreach ($this->blackList as $_b) {
+			if (strpos($request->requestedUrl, $_b) === 0) {
+				$request->unauthorized = TRUE;
+				$kernel->signal('access.denied', $user, $args);
+				return;
+			}
+		}
+
 		if ($user->isAnonymous()) {
 			$request->unauthorized = TRUE;
-			$args = array(
-				'request' => $request,
-				'user'    => $user
-			);
 			$kernel->signal('access.denied', $user, $args);
 		}
 	}
